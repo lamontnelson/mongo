ba40a6a276304b32792c086e5e21b613f13d61a2
Andrew Morrow <acm@mongodb.com>
2019-07-15 13:51:17 -0400

    Add SCons integration for opentracing-cpp

---
+++ src/third_party/shim_opentracing_cpp.cpp
@@ -0,0 +1,3 @@
+// This file intentionally blank.  shim_opentracing_cpp.cpp is part of the
+// third_party/opentracing-cpp library, which is just a placeholder for forwarding
+// library dependencies.
