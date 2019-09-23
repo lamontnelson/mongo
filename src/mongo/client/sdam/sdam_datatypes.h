8aca328dc70ebd0e0bbb857393a909e05c2740a3
Lamont Nelson <lamont.nelson@mongodb.com>
2019-09-23 14:41:09 -0400

    format

--- src/mongo/client/sdam/sdam_datatypes.h
+++ src/mongo/client/sdam/sdam_datatypes.h
@@ -37,7 +37,13 @@
 #include "mongo/util/duration.h"
 
 namespace mongo::sdam {
-enum class TopologyType { kSingle, kReplicaSetNoPrimary, kReplicaSetWithPrimary, kSharded, kUnknown };
+enum class TopologyType {
+    kSingle,
+    kReplicaSetNoPrimary,
+    kReplicaSetWithPrimary,
+    kSharded,
+    kUnknown
+};
 
 enum class ServerType {
     kStandalone,
