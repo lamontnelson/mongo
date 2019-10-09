87fe7882ffb4ca7c0c6565be105bd6ca71662226
Lamont Nelson <lamont.nelson@mongodb.com>
2019-10-09 15:47:30 -0400

    add newlines and comments to server description

--- src/mongo/client/sdam/server_description.h
+++ src/mongo/client/sdam/server_description.h
@@ -56,27 +56,36 @@ public:
      */
     bool isEquivalent(const ServerDescription& other) const;
 
+    // server identity
     const ServerAddress& getAddress() const;
-    const boost::optional<std::string>& getError() const;
-    const boost::optional<IsMasterLatency>& getRtt() const;
-    const boost::optional<Date_t>& getLastWriteDate() const;
-    const boost::optional<repl::OpTime>& getOpTime() const;
     ServerType getType() const;
     const boost::optional<ServerAddress>& getMe() const;
+    const boost::optional<std::string>& getSetName() const;
+    const std::map<std::string, std::string>& getTags() const;
+
+    // network attributes
+    const boost::optional<std::string>& getError() const;
+    const boost::optional<IsMasterLatency>& getRtt() const;
+    const boost::optional<int>& getLogicalSessionTimeoutMinutes() const;
+
+    // server capabilities
+    int getMinWireVersion() const;
+    int getMaxWireVersion() const;
+    bool isDataBearingServer() const;
+
+    // server 'time' statistics
+    const Date_t getLastUpdateTime() const;
+    const boost::optional<Date_t>& getLastWriteDate() const;
+    const boost::optional<repl::OpTime>& getOpTime() const;
+
+    // topology membership
+    const boost::optional<ServerAddress>& getPrimary() const;
     const std::set<ServerAddress>& getHosts() const;
     const std::set<ServerAddress>& getPassives() const;
     const std::set<ServerAddress>& getArbiters() const;
-    const std::map<std::string, std::string>& getTags() const;
-    const boost::optional<std::string>& getSetName() const;
     const boost::optional<int>& getSetVersion() const;
     const boost::optional<OID>& getElectionId() const;
-    const boost::optional<ServerAddress>& getPrimary() const;
-    const Date_t getLastUpdateTime() const;
-    const boost::optional<int>& getLogicalSessionTimeoutMinutes() const;
-    int getMinWireVersion() const;
-    int getMaxWireVersion() const;
 
-    bool isDataBearingServer() const;
     BSONObj toBson() const;
     std::string toString() const;
 
@@ -90,45 +99,60 @@ private:
     // is not the server's ismaster.me field, in the case that the server reports an address
     // different from the address the client uses.
     ServerAddress _address;
+
     // error: information about the last error related to this server. Default null.
     boost::optional<std::string> _error;
+
     // roundTripTime: the duration of the ismaster call. Default null.
     boost::optional<IsMasterLatency> _rtt;
+
     // lastWriteDate: a 64-bit BSON datetime or null. The "lastWriteDate" from the server's most
     // recent ismaster response.
     boost::optional<Date_t> _lastWriteDate;
+
     // opTime: an ObjectId or null. The last opTime reported by the server; an ObjectId or null.
     // (Only mongos and shard servers record this field when monitoring config servers as replica
     // sets.)
     boost::optional<repl::OpTime> _opTime;
+
     // (=) type: a ServerType enum value. Default Unknown.
     ServerType _type;
+
     // (=) minWireVersion, maxWireVersion: the wire protocol version range supported by the server.
     // Both default to 0. Use min and maxWireVersion only to determine compatibility.
     int _minWireVersion = 0;
     int _maxWireVersion = 0;
+
     // (=) me: The hostname or IP, and the port number, that this server was configured with in the
     // replica set. Default null.
     boost::optional<ServerAddress> _me;
+
     // (=) hosts, passives, arbiters: Sets of addresses. This server's opinion of the replica set's
     // members, if any. These hostnames are normalized to lower-case. Default empty. The client
     // monitors all three types of servers in a replica set.
     std::set<ServerAddress> _hosts;
     std::set<ServerAddress> _passives;
     std::set<ServerAddress> _arbiters;
+
     // (=) tags: map from string to string. Default empty.
     std::map<std::string, std::string> _tags;
+
     // (=) setName: string or null. Default null.
     boost::optional<std::string> _setName;
+
     // (=) setVersion: integer or null. Default null.
     boost::optional<int> _setVersion;
+
     // (=) electionId: an ObjectId, if this is a MongoDB 2.6+ replica set member that believes it is
     // primary. See using setVersion and electionId to detect stale primaries. Default null.
     boost::optional<OID> _electionId;
+
     // (=) primary: an address. This server's opinion of who the primary is. Default null.
     boost::optional<ServerAddress> _primary;
+
     // lastUpdateTime: when this server was last checked. Default "infinity ago".
     boost::optional<Date_t> _lastUpdateTime = Date_t::min();
+
     // (=) logicalSessionTimeoutMinutes: integer or null. Default null.
     boost::optional<int> _logicalSessionTimeoutMinutes;
 
